6b6bf617595613777bf94cd0f68914ca299218b3
martavp <martavictoriaperez@gmail.com>
2019-12-16 16:22:44 +0100

    added description TYNDP capacities

---
+++ paper_graphics/TYNDP_transmission_capacities.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 16:10:15 2019
+
+@author: Marta
+"""
+
+#import geopandas as gpd
+#import pypsa
+import pandas as pd
+import numpy as np
+
+#https://tyndp.entsoe.eu/maps-data/
+#TYNDP 2016 Market modelling data
+df_raw = pd.read_excel('data/exsiting_infrastructure/TYNDP2016 market modelling data.xlsx',
+                       sheet_name='ref. transmission capacities',index_col=0)
+
+
+df = df_raw.copy()
+for index in df.index:
+    if len(index) >= 8:
+        df.drop(index=index,inplace=True)
+    if 8 > len(index):
+        bus0,bus1 = index.split('-')
+        df.rename(mapper={index:bus0[:2]+'-'+bus1[:2]},inplace=True)
+#%%
+df = df.groupby(df.index).sum()
+
+df.columns = [2020,2030]
+
+#df[2025] = df.mean(axis=1)
+
+#for year in np.arange(2035,2055,5):
+#    df[year] = df[2030]
+
+#df = df.reindex(columns=np.arange(2020,2055,5))
+
+df.to_csv('data/exsiting_infrastructure/TYNDP2016.csv')
+#%%
+#write latex table
+import pandas as pd
+filename='../table_TYNDP.tex'
+
+             
+file = open(filename, 'w')
+df.drop(index=df.index[-1],inplace=True)
+nr=53 #number of raws in table
+for i,index in enumerate(df.index[0:nr]):
+    file.write(' ' + str(df.index[i]) + ' & ' + str(int(df[2020][df.index[i]])) 
+    + ' & ' + str(int(df[2030][df.index[i]])) + ' ')
+    file.write('&') 
+    file.write(' ' + str(df.index[i + nr]) + ' & ' + str(int(df[2020][df.index[i + nr]])) 
+    + ' & ' + str(int(df[2030][df.index[i + nr]])) + ' ')
+    file.write('&') 
+    file.write(' ' + str(df.index[i + 2*nr]) + ' & ' + str(int(df[2020][df.index[i + 2*nr]])) 
+    + ' & ' + str(int(df[2030][df.index[i + 2*nr]])) + ' ')
+    file.write('\\') 
+    file.write('\\') 
+file.close()    
+
+    
