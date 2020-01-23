Add biomass EOP and decentral air-sourced heat pump. Fix errors in units.

paper_graphics/data/costs/tables_in_latex.py
@@ -144,7 +144,7 @@ dic_units={'EUR/kWel':'\EUR/kW$_{el}$',
            'EUR/MW':'\EUR/MW',
            'USD/kWel':'USD/kW$_{el}$',
            'USD/kWh':'USD/kWh',
-           'EUR/kW(h)': '\EUR/kWh'}
+           'EUR/kWh': '\EUR/kWh'}
 for technology in technologies:
     file.write(' ' +name[technology] + ' & ')    
     file.write(dic_units[costs.loc[idx[technology,'investment'],'unit']]+ ' & ' )
@@ -153,6 +153,8 @@ for technology in technologies:
         costs_year = pd.read_csv('outputs/costs_' + str(year) +'.csv',index_col=list(range(2))).sort_index()
         if technology=='hydrogen storage':
             file.write(str(round(costs_year.loc[idx[technology,'investment'],'value'],1))+ ' & ' )
+        elif technology=='central water tank storage':
+            file.write(str(int(np.ceil(costs_year.loc[idx[technology,'investment'],'value'])))+ ' & ' )
         else:
             file.write(str(int(costs_year.loc[idx[technology,'investment'],'value']))+ ' & ' )
         